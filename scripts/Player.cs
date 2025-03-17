d42d8c9c2ea6052f8fb46e8bc9450c7d8a7bfc4e
burdenburied <scythmeisei@gmail.com>
2025-03-17 22:33:46 +0200

    Player now uses C# script

--- scripts/Player.cs
+++ scripts/Player.cs
@@ -1,12 +1,21 @@
 using Godot;
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 public partial class Player : CharacterBody2D
 {
-	public const float Speed = 300.0f;
-	public const float JumpVelocity = -400.0f;
+	public const float Speed = 130.0f;
+	public const float JumpVelocity = -300.0f;
+	private AnimatedSprite2D animated_sprite;
+	private AudioStreamPlayer2D jump_sfx;
 
-	public override void _PhysicsProcess(double delta)
+    public override void _Ready()
+    {
+        animated_sprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+		jump_sfx = GetNode<AudioStreamPlayer2D>("AudioStreamPlayer2D");
+    }
+
+    public override void _PhysicsProcess(double delta)
 	{
 		Vector2 velocity = Velocity;
 
@@ -17,14 +26,16 @@ public partial class Player : CharacterBody2D
 		}
 
 		// Handle Jump.
-		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
+		if (Input.IsActionJustPressed("jump") && IsOnFloor())
 		{
 			velocity.Y = JumpVelocity;
+			jump_sfx.Play();
 		}
+
 
 		// Get the input direction and handle the movement/deceleration.
 		// As good practice, you should replace UI actions with custom gameplay actions.
-		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+		Vector2 direction = Input.GetVector("move_left", "move_right", "ui_up", "ui_down");
 		if (direction != Vector2.Zero)
 		{
 			velocity.X = direction.X * Speed;
@@ -32,6 +43,33 @@ public partial class Player : CharacterBody2D
 		else
 		{
 			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+		}
+
+		// Flip sprites
+		if (direction > new Vector2(0, 0))
+		{
+			animated_sprite.FlipH = false;
+		}
+		if (direction < new Vector2(0, 0))
+		{
+			animated_sprite.FlipH = true;
+		}
+
+		//Run animation
+		if (IsOnFloor())
+		{
+			if (direction != Vector2.Zero)
+			{
+				animated_sprite.Play("run");
+			}
+			else
+			{
+				animated_sprite.Play("idle");
+			}
+		}
+		else 
+		{
+			animated_sprite.Play("jump");
 		}
 
 		Velocity = velocity;
